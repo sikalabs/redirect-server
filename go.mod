da8985051e44ba7e64cde59d5f9fd8b4ea08f2ce
Ondrej Sika <ondrej@ondrejsika.com>
2024-04-22 22:41:17 +0200

    feat: Add logging int json usin zerolog

--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module github.com/sikalabs/redirect-server
 
 go 1.19
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/rs/zerolog v1.32.0
+	github.com/spf13/cobra v1.2.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,18 +3,26 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func Server(baseTarget string, preservePath bool) error {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	hostname, _ := os.Hostname()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		target := baseTarget
 		if preservePath {
 			target = fmt.Sprintf("%s%s", baseTarget, r.URL.Path)
 		}
+		log.Info().Str("hostname", hostname).Str("path", r.URL.Path).Msg(r.Host + r.URL.Path + " -> " + target)
 		http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 	})
 
 	port := 8000
-	fmt.Printf("Server started on 0.0.0.0:%d, see http://127.0.0.1:%d\n", port, port)
+	log.Info().Str("hostname", hostname).Msgf("Server started on 0.0.0.0:%d, see http://127.0.0.1:%d", port, port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
